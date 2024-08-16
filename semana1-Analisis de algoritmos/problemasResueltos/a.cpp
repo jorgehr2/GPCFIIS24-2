674a8c7fdd74a0950af3e75e15063903a6ebcec4
TuNuevoNombreDeUsuario <tu@nuevo.email>
2024-08-15 21:43:07 -0500

    problema a

---
+++ semana1-Analisis de algoritmos/problemasResueltos/a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+ll arr[200000 + 2];
+int main() {
+    ll n;
+    cin >> n;
+    for (ll i = 1; i < n; i++) {
+        ll num;
+        cin >> num;
+        arr[num]++;
+    }
+    for (ll i = 1; i <= n; i++) {
+        if (arr[i] == 0) {
+            cout << i << endl;
+            break;
+        }
+    }
+    return 0;
+}
